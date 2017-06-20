c57526298349f37ca666733618fa52138a19a937
Robert L. Read <read.robert@gmail.com>
2017-06-20 09:23:03 -0500

    robs draft

--- app/facedetect.py
+++ app/facedetect.py
@@ -8,18 +8,22 @@ face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 cap = cv2.VideoCapture(0)
 
 while True:
+   faces = getFace()
+        cv2.rectangle(img, (x,y), (x+w, y+h), (255,0,0), 2)
+    cv2.imshow('img', img)
+    k = cv2.waitKey(30) & 0xff
+    if k == 27:
+        break
+    
+def getFace:
     ret, img = cap.read()
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     faces = face_cascade.detectMultiScale(gray, 1.3, 5)
     for (x,y,w,h) in faces:
         center = x+(w/2)
         print "this is x: %d" % center
-        cv2.rectangle(img, (x,y), (x+w, y+h), (255,0,0), 2)
-    cv2.imshow('img', img)
-    k = cv2.waitKey(30) & 0xff
-    if k == 27:
-        break
-
+    ret faces;
+    
 
 
 cap.release()
